b33622e6ebea3819d11d756cf3ba5d074102f303
akshayvsanthosh <akshaysanthosh12345@gmail.com>
2024-09-18 14:49:53 +0530

    astra-admin completed

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import Header from '../components/Header'
+import Header from '../Components/Header'
 import Grid from '@mui/material/Grid2';
 import Footer from '../components/Footer';
 import { getAllGrievanceAPI, updateGrievanceStatusAPI } from '../Services/allApi';
